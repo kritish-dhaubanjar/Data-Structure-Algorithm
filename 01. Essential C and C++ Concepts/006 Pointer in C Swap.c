4064c68f82c01931670551a041b36c463b443851
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2021-02-01 18:30:24 +0545

    Pointer Swap

---
+++ 01. Essential C and C++ Concepts/006 Pointer in C Swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    printf("a=%d,b=%d\n", a, b);
+    swap(&a, &b);
+    printf("a=%d,b=%d", a, b);
+
+    return 0;
+}
