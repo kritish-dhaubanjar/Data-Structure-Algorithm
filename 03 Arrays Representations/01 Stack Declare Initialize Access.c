31684a80dda09092966c5d05d2e89981a32d3543
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2021-02-04 21:02:30 +0545

    Static Array

---
+++ 03 Arrays Representations/01 Stack Declare Initialize Access.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    //Stack
+
+    //Declare Initialize
+    int A[5];
+    int B[] = {0, 1, 2, 3, 4, 5};
+    int C[10] = {1, 1};
+
+    //Access
+    printf("%d", C[1]);
+    printf("%d", 1 [C]);
+    printf("%d", *(C + 1));
+    return 0;
+}
---
+++ 03 Arrays Representations/02 Stack Declare Initialize Access.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    //Stack
+    int A[n];
+    return 0;
+}
